562ced4807b3da87147ea02eaf698d76e4e7a7d1
Milton Petitfour <miltonriver66@gmail.com>
2023-12-30 23:03:09 -0300

    Desafio 6 para cumplir, falta aplicar handlebars y websocket

---
+++ src/routers/carts.router.js
@@ -0,0 +1,71 @@
+import { Router } from "express";
+import carts from "../CartsManager.js";
+
+const cartsRouter = Router();
+
+cartsRouter.get('/:cid', async (req, res) => {
+    const { cid } = req.params
+    try {
+        const cartId = await carts.getCartById(Number(cid))
+        
+        if (cartId.length !== 0){
+            res.status(200).send({
+                status: "succes",
+                cartId
+            })
+        }
+        
+    } catch (error) {
+        res.status(400).send({
+            status: 'error',
+            message: 'El carrito solicitado no existe o está vacío'
+        })
+        
+    }
+
+})
+
+cartsRouter.post('/', async (req, res) => {
+    try {    
+        const newCarts = await carts.createCart()
+        console.log(newCarts)
+        res.status(201).send({
+            status: "success",
+            message: 'El producto se ha agregado al carrito',
+            newCarts
+        })
+        
+    } catch (error) {
+        res.status(500).send({
+            status: 'error',
+            message: 'Error al agregar el producto al carrito',
+            error: error.message
+        })        
+    }
+
+})
+
+cartsRouter.post('/:cid/product/:pid', async (req, res) => {
+    const { cid, pid } = req.params
+    const cartId = await carts.addProductToCart(Number(cid), Number(pid))
+
+    try {
+            res.status(200).send({
+                status: "succes",
+                message: 'Producto agregado al carrito con éxito',
+                payload: await cartId,
+                //carts
+            })
+        
+    } catch (error) {
+        res.status(404).send({
+            status: 'error',
+            mesagge: 'El carrito está vacio',
+            carts
+        })
+        
+    }
+
+})
+
+export default cartsRouter
